3b848e123039d40358803684868fcb4679a3e4ed
acedsp4de <acedsp4de@gmail.com>
2006-01-08 12:00:00 +0000

    Commit on 2006-01-08

---
+++ imphnen-chart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git init
+git remote add origin https://github.com/acesp4de/reforestation-script.git
+git branch -M main
+
+while read current_date; do
+  git add .
+  GIT_AUTHOR_DATE="$current_date 12:00:00" GIT_COMMITTER_DATE="$current_date 12:00:00" \
+  git commit -m "Commit on $current_date"
+  
+  git push origin main
+done < dates.txt
